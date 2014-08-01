053977021ea43a59f38eb0e0b3d5c238f3146d56
ragarwal91 <ranjan@ranjandesign.com>
2014-08-01 16:13:42 -0400

    added app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
   # Welcome
-  
+  get "/" => 'welcome#index' as: root
+
+  resources :users, except: [:index]
+  resources :restaurants, only: [:index, :show]
+  resources :reviews, except: [:show, :index]
+  resources :favorites, except: [:show, :index, :destroy]
+
 end
