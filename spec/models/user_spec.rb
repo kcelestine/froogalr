ef2de43eb296593072528361766e8d17e76f70ec
aaackerman <aaackerman@yahoo.com>
2014-08-04 11:16:09 -0400

    added validations for email address, and relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,11 @@ it { is_expected.to validate_presence_of(:username) }
 it { is_expected.to validate_uniqueness_of(:username) }
 it { is_expected.to validate_presence_of(:first_name) }
 it { is_expected.to validate_presence_of(:last_name) }
+it { is_expected.to validate_presence_of(:email_address) }
+it { is_expected.to validate_uniqueness_of(:email_address) }
 it { is_expected.to validate_presence_of(:password) }
 
 it { is_expected.to have_many(:reviews) }
+it { is_expected.to have_and_belong_to_many(:restaurants) }
 it { is_expected.to have_secure_password } 
 end
