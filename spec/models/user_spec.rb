bd124bc044802cc3c2f266938de3bab67539c846
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-08-05 11:34:47 -0700

    user model tests all passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ describe User do
   it "has a valid factory" do
     FactoryGirl.build(:user).should be_valid
   end
-  it { expect(subject).to validate_presence_of(:username) }
-  it { expect(subject).to validate_presence_of(:first_name) }
-  it { expect(subject).to validate_presence_of(:last_name) }
-  it { expect(subject).to validate_presence_of(:email) }
-  it { expect(subject).to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
 
-  it { expect(subject).to have_many(:reviews) }
-  it { expect(subject).to have_and_belong_to_many(:restaurants) }
+  it { is_expected.to have_many(:reviews) }
+  it { is_expected.to have_and_belong_to_many(:restaurants) }
 end
