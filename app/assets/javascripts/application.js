4d996e9647fe018bc157a8329e33d3ccf15e74df
ragarwal91 <ranjan@ranjandesign.com>
2014-08-05 16:20:26 -0400

    map changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,7 +28,11 @@ function initialize() {
 function loadScript() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=initialize';
   document.body.appendChild(script);
 }
+
+function unfavorite() {
+  var unfavoriteButton = $('#favorite-button').val('Unfavorite').attr('id', 'unfavorite-button');
+
+}
+unfavorite();
