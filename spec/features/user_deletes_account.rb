b7f033d1b48cdcddeef4f0d3eec653eac9cfaa48
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-08-04 21:48:42 -0700

    user story: delete

---
+++ spec/features/user_deletes_account.rb
@@ -0,0 +1,9 @@
+# Feature: Delete account
+#  As a user
+#  I want to delete my account
+#  So that all of my reviews and favorites are deleted
+
+feature 'User deletes account' do
+  scenario 'and all of the data is removed from the database' do
+  end
+end
