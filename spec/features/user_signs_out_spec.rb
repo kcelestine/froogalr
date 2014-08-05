cb91e52ce09199e258ce205f124007ab2462f6aa
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-08-04 21:39:03 -0700

    user story: logout

---
+++ spec/features/user_signs_out_spec.rb
@@ -0,0 +1,13 @@
+# Feature: Log out
+#  As a user
+#  I want to logout
+#  So someone else can log in
+
+feature 'User logs out' do
+
+  scenario 'sucessfully' do
+  end
+
+  scenario 'and someone else can log in' do
+  end
+end
