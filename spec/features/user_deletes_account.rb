c6a70df4e13dbda52461b1287ab9e7cd4c0ba108
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-08-06 11:36:28 -0700

    removed features from development branch

--- spec/features/user_deletes_account.rb
+++
@@ -1,9 +0,0 @@
-# Feature: Delete account
-#  As a user
-#  I want to delete my account
-#  So that all of my reviews and favorites are deleted
-
-feature 'User deletes account' do
-  scenario 'and all of the data is removed from the database' do
-  end
-end
--- spec/features/user_views_restaurant_profile_spec.rb
+++
@@ -1,6 +0,0 @@
-# Feature: View Restaurant
-#  As a user
-#  I want to view a restaurants profile page
-#  So I can view their ratings and reviews
-
-
