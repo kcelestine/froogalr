794c78bc3192ee4862b00c2e85de48f1a4d6cf38
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-08-07 22:09:15 -0700

    attempted to add controller tests. absolutely failed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-
+  describe 'POST #create' do
+    context 'when creating a user' do
+      it 'assigns @users' do
+      end
+    end
+  end
 end
